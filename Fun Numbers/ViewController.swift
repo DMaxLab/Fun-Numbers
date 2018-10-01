f0938257163d1cdc1185a9a7f1a6e7cd2d8f192a
Thobio Joseph <jthobio2@gmail.com>
2018-10-01 18:33:07 +0530

    Initial Commit

---
+++ Fun Numbers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Fun Numbers
+//
+//  Created by Thobio on 01/10/18.
+//  Copyright © 2018 Zerone Consulting. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
